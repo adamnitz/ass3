7855a80a083495b33bc1ed2df5ea52cc4dc7a6cf
Meitar <meitargo@post.bgu.ac.il>
2020-12-29 17:31:44 +0200

    changes in dataBase

--- spl-net/src/main/java/bgu/spl/net/api/MessageEncoderDecoderImpl.java
+++ spl-net/src/main/java/bgu/spl/net/api/MessageEncoderDecoderImpl.java
@@ -9,52 +9,85 @@ public class MessageEncoderDecoderImpl implements MessageEncoderDecoder{
     private byte[] bytes = new byte[1<<10];//check how many bites
     private int len=0;
     short opCode;
-    boolean first=true;
+    int fullOpcode=0;
     int zeroCounter = 0;
     int bytesCounter = 0;
 
     public String decodeNextByte(byte nextByte) {
-        if(first){
-            opCode=nextByte;
+        //read the opcode
+        if(fullOpcode==0) {
+            opCode = nextByte;
+            fullOpcode++;
             bytesCounter++;
-            first=false;
         }
-        else {
+        if(fullOpcode==1){
+            opCode = (short) (opCode+nextByte);
+            fullOpcode++;
+            bytesCounter++;
+        }
+
+        //know when the message over by the opcode
+        else if(fullOpcode == 2) {
             if (opCode == 1) {
-                if (zeroCounter == 2) {
+                if (zeroCounter == 2)
                     return popString();
-                }
-                if (nextByte == '\0') {
+                if (nextByte == '\0')
                     zeroCounter++;
-                }
             } else if (opCode == 2) {
-                if (zeroCounter == 2) {
+                if (zeroCounter == 2)
                     return popString();
-                }
-                if (nextByte == '\0') {
+                if (nextByte == '\0')
                     zeroCounter++;
-                }
             } else if (opCode == 3) {
-                if (zeroCounter == 2) {
+                if (zeroCounter == 2)
                     return popString();
-                }
-                if (nextByte == '\0') {
+                if (nextByte == '\0')
                     zeroCounter++;
-                }
             } else if (opCode == 4) {
-                if (bytesCounter == 2) {
+                if (bytesCounter == 2)
                     return popString();
-                }
                 bytesCounter++;
             } else if (opCode == 5) {
-                if (bytesCounter == 4) {
+                if (bytesCounter == 4)
                     return popString();
-                }
+                bytesCounter++;
             }
             else if (opCode == 6) {
-                if (bytesCounter == 4) {
+                if (bytesCounter == 4)
                     return popString();
-                }
+                bytesCounter++;
+            }
+            else if(opCode==7){
+                if (bytesCounter == 4)
+                    return popString();
+                bytesCounter++;
+            } else if (opCode == 8) {
+                if(zeroCounter==1)
+                    return popString();
+                if (nextByte == '\0')
+                    zeroCounter++;
+            } else if(opCode == 9){
+                if (bytesCounter == 4)
+                    return popString();
+                bytesCounter++;
+            } else if(opCode == 10) {
+                if (bytesCounter == 4)
+                    return popString();
+                bytesCounter++;
+            }
+            else if(opCode == 11) {
+                if (bytesCounter == 2)
+                    return popString();
+                bytesCounter++;
+            }else if (opCode == 12) {
+                if(zeroCounter==1)
+                    return popString();
+                if (nextByte == '\0')
+                    zeroCounter++;
+            } else if(opCode == 13) {
+                if (bytesCounter == 4)
+                    return popString();
+                bytesCounter++;
             }
 
         }
@@ -62,9 +95,10 @@ public class MessageEncoderDecoderImpl implements MessageEncoderDecoder{
         return null;
     }
 
+
     @Override
     public byte[] encode(String message) {
-        return new byte[0];
+        return (message+ "\n").getBytes();
     }
 
     public void pushByte(byte nextByte){
