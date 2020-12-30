8a75d3404e8568a02f034c39986b5dd3ddc13c5f
Meitar <meitargo@post.bgu.ac.il>
2020-12-30 22:09:17 +0200

    changes in MessageEncoderDecoderImpl

--- spl-net/src/main/java/bgu/spl/net/api/MessagingProtocolImpl.java
+++ spl-net/src/main/java/bgu/spl/net/api/MessagingProtocolImpl.java
@@ -7,7 +7,8 @@ public class MessagingProtocolImpl implements MessagingProtocol{
 
     private Database myData = Database.getInstance();
     private String myUser= null;
-    private Message resopnse = null;
+    private Message response = null;
+    private boolean shouldTerminate = false;
 
     @Override
     public Message process(String msg) {
@@ -26,32 +27,33 @@ public class MessagingProtocolImpl implements MessagingProtocol{
             password = msg.substring(firstCharOfPass,counter);
             switch (opCode){
                 case 1:
-                   resopnse = myData.adminReg(userName,password);
+                    response = myData.adminReg(userName,password);
                     break;
                 case 2:
-                    resopnse = myData.studentReg(userName,password);
+                    response = myData.studentReg(userName,password);
                     break;
                 case 3:
-                    resopnse = myData.logIn(userName,password);
+                    response = myData.logIn(userName,password);
                     myUser = userName;
                     break;
             }
         }
         if(opCode==4)
-            resopnse = myData.logOut(myUser);
+            response = myData.logOut(myUser);
 
        if(opCode == 5||opCode == 6||opCode == 7||opCode == 9||opCode == 10){
            int courseNum = Integer.parseInt(msg.substring(2,msg.length()));
             switch(opCode){
-                case 5: resopnse = myData.courseReg(myUser,courseNum);//todo: check when test if myuser is not null
+                case 5: response = myData.courseReg(myUser,courseNum);//todo: check when test if myuser is not null
                     break;
-                case 6: resopnse = myData.kdamCheck(myUser,courseNum);
+                case 6: response = myData.kdamCheck(myUser,courseNum);
                     break;
-                case 7: resopnse = myData.courseStat(courseNum);
+                case 7: response = myData.courseStat(courseNum);
                     break;
-                case 9: resopnse = myData.isRegistered(myUser,courseNum);
+                case 9: response = myData.isRegistered(myUser,courseNum);
                     break;
-                case 10: resopnse =myData.unRegister(myUser,courseNum);
+                case 10: response =myData.unRegister(myUser,courseNum);
+                shouldTerminate=true;
                     break;
             }
 
@@ -61,21 +63,20 @@ public class MessagingProtocolImpl implements MessagingProtocol{
                   counter ++;
               userName = msg.substring(2,counter);
               switch (opCode){
-                  case 8:resopnse = myData.studentStat(userName);
+                  case 8:response = myData.studentStat(userName);
                         break;
               }
           }
           if(opCode==11){
-              resopnse = myData.myCourses(myUser);
+              response = myData.myCourses(myUser);
           }
        }
 
-        return resopnse;
+        return response;
     }
 
     @Override
     public boolean shouldTerminate() {
-
-        return false;
+        return shouldTerminate;
     }
 }
