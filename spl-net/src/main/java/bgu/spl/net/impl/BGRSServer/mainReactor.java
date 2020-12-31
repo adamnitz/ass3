5d77c8647b51067042ee3170337264822bc228d7
Meitar <meitargo@post.bgu.ac.il>
2020-12-31 13:15:39 +0200

    changes in mainReactor

--- spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/mainReactor.java
+++ spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/mainReactor.java
@@ -9,14 +9,12 @@ import java.io.IOException;
 
 public class mainReactor {
     public static void main(String[] args){
-        try(Server<Message> srv = Server.reactor(3, Integer.parseInt(args[0]),() -> new MessagingProtocolImpl(), () -> new MessageEncoderDecoderImpl()))
+        try(Server<Message> srv = Server.reactor(Integer.parseInt(args[1]), Integer.parseInt(args[0]),() -> new MessagingProtocolImpl(), () -> new MessageEncoderDecoderImpl()))
         {
             srv.serve();
         }
         catch (IOException e) {
             System.out.println("IOException in mainTpc");
         }
-
-
     }
 }
